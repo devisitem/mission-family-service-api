a05d0f71909fb29dd1cc8eb5fac42570c5f46f76
Kimchi Developer <65699391+Kimchi-dev@users.noreply.github.com>
2021-06-22 22:50:36 +0900

    configure WebConfig

--- src/main/java/me/missionfamily/web/mission_family_be/config/WebMvcConfig.java
+++ src/main/java/me/missionfamily/web/mission_family_be/config/WebMvcConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -24,25 +25,37 @@ public class WebMvcConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf()
-                .disable()
-                .authorizeRequests()
-                .antMatchers("/api/**").permitAll()
+                .csrf().disable()
+
+                .exceptionHandling()
                 .and()
-                .formLogin()
-                .disable()
-                .logout();
+
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+                .and()
+
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+
+                .authorizeRequests()
+                .antMatchers("/api/users/login").permitAll()
+                .anyRequest()
+                .authenticated();
+
+
     }
 
 
-    @Bean
-    public CustomBasicAuthenticationEntryPoint authenticationEntryPoint(){
-        return new CustomBasicAuthenticationEntryPoint();
-    }
+
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-            web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+            web
+                    .ignoring()
+                    .antMatchers("/h2-console/**",
+                            "/favicon.ico");
     }
 
     @Bean
