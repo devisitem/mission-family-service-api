9e54053060b7e9692a177673aacc3457e8489cb0
Kimchi Developer <65699391+Kimchi-dev@users.noreply.github.com>
2021-10-10 22:58:05 +0900

    Message Model builder

--- src/main/java/me/missionfamily/web/mission_family_be/common/transactional/model/MessageModel.java
+++ src/main/java/me/missionfamily/web/mission_family_be/common/transactional/model/MessageModel.java
@@ -1,8 +1,18 @@
 package me.missionfamily.web.mission_family_be.common.transactional.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MessageModel {
     String title;
     String content;
     String group;
     String topic;
+    LocalDate createTime;
 }
