68829c9bc76b933c011bf8572569fd789e47bcf8
Kimchi-dev <tnine923@gmail.com>
2021-12-20 00:15:46 +0900

    thread local get() method always return null, so that is always create new context holder instance

--- src/main/java/me/missionfamily/web/mission_family_be/MissionFamilyBeApplication.java
+++ src/main/java/me/missionfamily/web/mission_family_be/MissionFamilyBeApplication.java
@@ -2,7 +2,7 @@ package me.missionfamily.web.mission_family_be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 public class MissionFamilyBeApplication {
